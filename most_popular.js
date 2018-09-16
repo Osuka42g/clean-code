1d14566f9119fac4f84d0bb007803598c7d176af
Osuka42g <osuka42@gmail.com>
2018-09-16 18:57:25 -0500

    Fix lint

--- most_popular.js
+++ most_popular.js
@@ -1,45 +1,45 @@
 function mostPopularName(votes) {
-    var votesPerName = votesCounting(votes)
-    var mostVotedName = nameWithHigherScore(votesPerName)
+    var votesPerName = votesCounting(votes);
+    var mostVotedName = nameWithHigherScore(votesPerName);
 
-    return mostVotedName
+    return mostVotedName;
 }
 
 function votesCounting(votes) {
-    var votesPerName = []
+    var votesPerName = [];
 
     votes.forEach(function (name) {
         if (!votesPerName[name]) {
-            votesPerName[name] = 1
+            votesPerName[name] = 1;
         }
-        votesPerName[name] = votesPerName[name] + 1
-    })
+        votesPerName[name] = votesPerName[name] + 1;
+    });
 
-    return votesPerName
+    return votesPerName;
 }
 
 function nameWithHigherScore(votesPerName) {
-    var mostVotedName = ""
-    var highestScore = 0
-    var names = Object.keys(votesPerName)
+    var mostVotedName = "";
+    var highestScore = 0;
+    var names = Object.keys(votesPerName);
 
     names.forEach(function (name) {
         if (votesPerName[name] > highestScore) {
-            highestScore = votesPerName[name]
-            mostVotedName = name
+            highestScore = votesPerName[name];
+            mostVotedName = name;
         }
-    })
+    });
 
-    return mostVotedName
+    return mostVotedName;
 }
 
 function printMostPopularName(names) {
-    console.log(mostPopularName(names))
+    console.log(mostPopularName(names));
 }
 
-var names = ["Jessica", "Mauricia", "Jacky", "Jacky", "Heriberta", "Romualdo", "Mauricia", "Heriberta", "Jacky"]
+var names = ["Jessica", "Mauricia", "Jacky", "Jacky", "Heriberta", "Romualdo", "Mauricia", "Heriberta", "Jacky"];
 
-var mostPopular = mostPopularName(names)
-console.log(mostPopular)
+var mostPopular = mostPopularName(names);
+console.log(mostPopular);
 
-printMostPopularName(names)
+printMostPopularName(names);
