5b6b6b708aa30498c606291e307a4232700184d3
Osuka42g <osuka42@gmail.com>
2018-09-16 17:32:26 -0500

    Split into smaller functions

--- most_popular.js
+++ most_popular.js
@@ -1,18 +1,29 @@
 function mostPopularName(votes) {
+    var votesPerName = votesCounting(votes)
+    var mostVotedName = nameWithHigherScore(votesPerName)
+
+    return mostVotedName
+}
+
+function votesCounting(votes) {
     var votesPerName = []
-    votes.forEach(function(name) {
-        if (typeof(votesPerName[name]) !== "undefined") {
-            votesPerName[name] = votesPerName[name] + 1
-        } else {
+
+    votes.forEach(function (name) {
+        if (!votesPerName[name]) {
             votesPerName[name] = 1
         }
+        votesPerName[name] = votesPerName[name] + 1
     })
 
+    return votesPerName
+}
+
+function nameWithHigherScore(votesPerName) {
     var mostVotedName = ""
     var highestScore = 0
     var names = Object.keys(votesPerName)
-    
-    names.forEach(function(name) {
+
+    names.forEach(function (name) {
         if (votesPerName[name] > highestScore) {
             highestScore = votesPerName[name]
             mostVotedName = name
