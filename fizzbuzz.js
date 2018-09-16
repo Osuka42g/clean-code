66e74d83a9718586bb76b2fccdf0c570fca617f3
Osuka42g <osuka42@gmail.com>
2018-09-16 18:56:01 -0500

    Fix lint

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,14 @@
 function printWizeLineFizzBuzz() {
     for (var i = 1; i <= 100; i++) {
-        console.log(textForValue(i))
+        console.log(textForValue(i));
     }
 }
 
 function textForValue(n) {
-    if (n % 3 === 0 && n % 5 === 0) return "WizeLine"
-    if (n % 3 === 0) return "Wize"
-    if (n % 5 === 0) return "Line"
-    return n
+    if (n % 3 === 0 && n % 5 === 0) return "WizeLine";
+    if (n % 3 === 0) return "Wize";
+    if (n % 5 === 0) return "Line";
+    return n;
 }
 
-printWizeLineFizzBuzz()
+printWizeLineFizzBuzz();
