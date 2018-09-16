bba3f3efa3c19ce870a54f5f0b571980cfb5a254
Osuka42g <osuka42@gmail.com>
2018-09-16 18:30:34 -0500

    Clean up

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,14 @@
-function fizzbuzz() {
+function printWizeLineFizzBuzz() {
     for (var i = 1; i <= 100; i++) {
-        var temp = ""
-        if (i % 3 === 0) {
-            temp += "Wize"
-        }
-        if (i % 5 === 0) {
-            temp += "Line"
-        }
-        console.log(temp ? temp : i)
+        console.log(textForValue(i))
     }
 }
 
-fizzbuzz()
+function textForValue(n) {
+    if (n % 15 === 0) return "WizeLine"
+    if (n % 5 === 0) return "Wize"
+    if (n % 3 === 0) return "Line"
+    return n
+}
+
+printWizeLineFizzBuzz()
